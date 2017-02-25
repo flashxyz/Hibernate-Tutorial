9199e287a49d4023244ea6b63633b319af4ca622
elya-PC\elya <flashxyz@gmail.com>
2017-02-25 18:59:00 +0200

    EntityManagerFactory of JPA with Hibernate - better use of it

--- src/main/java/data/Application.java
+++ src/main/java/data/Application.java
@@ -16,28 +16,41 @@ public class Application {
         //Session session = HibernateUtil.getSessionFactory().openSession();
         //session.getTransaction().begin();
 
-        //JPA
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("infinite-finances");
-        EntityManager em = emf.createEntityManager();
 
-        EntityTransaction tx = em.getTransaction();
-        tx.begin();
+        //JPA as interface - Hibernate as implementation
+        EntityManagerFactory factory = null;
+        EntityManager em = null;
+        EntityTransaction tx = null;
+        try{
+            factory = Persistence.createEntityManagerFactory("infinite-finances");
+            em = factory.createEntityManager();
+            tx = em.getTransaction();
+            tx.begin();
 
-        User user = new User();
-        user.setBirthDate(new Date());
-        user.setCreatedDate(new Date());
-        user.setCreatedBy("eliya Bar On test");
-        user.setEmailAddress("asd@asd.com");
-        user.setFirstName("Morgane - JPA");
-        user.setLastName("Mohr - JPA");
-        user.setLastUpdatedBy("Flash");
-        user.setLastUpdatedDate(new Date());
+            // Here we start creating entities/getting entities from DB...
+            User user = new User();
+            user.setBirthDate(new Date());
+            user.setCreatedDate(new Date());
+            user.setCreatedBy("eliya Bar On test");
+            user.setEmailAddress("asd@asd.com");
+            user.setFirstName("Morgane - JPA");
+            user.setLastName("Mohr - JPA");
+            user.setLastUpdatedBy("Flash");
+            user.setLastUpdatedDate(new Date());
 
+            em.persist(user);
 
-        em.persist(user);
-        tx.commit();
-        em.close();
-        emf.close();
+            //commit to DB
+            tx.commit();
+
+        }catch (Exception e){
+            // If we have a problem - roll back to the start.
+            tx.rollback();
+        }finally {
+            em.close();
+            factory.close();
+        }
+
 
         //session.save(user);
 
